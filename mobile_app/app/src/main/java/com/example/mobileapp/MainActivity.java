ffb0f5f816701f2165e3c2e8a77b9fea586dd5c8
rizza <44263376+rizza99@users.noreply.github.com>
2021-04-23 15:37:46 +0200

    mobile app that i made

---
+++ mobile_app/app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button loginButton;
+    Button registorButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.mainLogin);
+        registorButton = (Button) findViewById(R.id.mainRegister);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+        registorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
+
+    }
+}
