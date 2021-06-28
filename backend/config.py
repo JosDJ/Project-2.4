ef9905a7373eb6ac11f6ea99476f3354b4933eff
lordhessel2 <h.van.der.heide@st.hanze.nl>
2021-06-28 13:21:48 +0200

    config

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,9 @@
-config = {
-    'DATABASE_URI' : 'postgresql://postgres:Pieter1998@localhost:5432/project-2.4'
-}
+# config = {
+#     'DATABASE_URI' : 'postgresql://postgres:Pieter1998@localhost:5432/project-2.4'
+# }
 # config = {
 #     'DATABASE_URI' : 'postgresql://postgres:642251s@localhost:5432/artestify'
 # }
+config = {
+    'DATABASE_URI' : 'postgresql://postgres:artestify@localhost:5432/artestify'
+}
