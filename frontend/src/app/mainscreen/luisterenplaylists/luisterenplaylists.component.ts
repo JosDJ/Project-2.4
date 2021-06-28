4c65bdf2dd29a45818976d2133ef1f8c92a84f56
Douwe Pieter Reitsma <d.p.reitsma@gmail.com>
2021-06-28 15:10:23 +0200

    css enzo

--- frontend/src/app/mainscreen/luisterenplaylists/luisterenplaylists.component.ts
+++ frontend/src/app/mainscreen/luisterenplaylists/luisterenplaylists.component.ts
@@ -10,10 +10,56 @@ import { Song } from 'src/app/interfaces/song';
   styleUrls: ['./luisterenplaylists.component.css']
 })
 export class LuisterenplaylistsComponent implements OnInit {
-  playlists: Playlist[] = [];
+  playlists: Playlist[] = [
+    {
+      id: 1,
+      title: "Playlist 1",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 2",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 3",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 4",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 5",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 6",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 7",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 8",
+      songs: []
+    },
+    {
+      id: 1,
+      title: "Playlist 9",
+      songs: []
+    },
+  ];
 
   constructor(private http: HttpClient) { 
-    http.get<Playlist[]>(`${environment.apiUrl}/playlists`).subscribe(playlists => this.playlists = playlists);
+    //http.get<Playlist[]>(`${environment.apiUrl}/playlists`).subscribe(playlists => this.playlists = playlists);
   }
 
   ngOnInit(): void {
