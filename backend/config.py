074def0c866406874b7c39d2b0981ad89d59948a
rizza99 <44263376+rizza99@users.noreply.github.com>
2021-06-29 13:39:26 +0200

    upload update

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 config = {
-    'DATABASE_URI' : 'postgresql://postgres:Pieter1998@localhost:5432/project-2.4'
+    'DATABASE_URI' : 'postgresql://postgres:12230@localhost:5432/project-2.4'
 }
 # config = {
 #     'DATABASE_URI' : 'postgresql://postgres:642251s@localhost:5432/artestify'
