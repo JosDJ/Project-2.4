09ddd4e151d05d79df45a4325f361f2b648f98ba
riffer10000 <stephan123@live.nl>
2021-06-30 11:12:56 +0200

    confiiggg

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,12 @@
 # config = {
 #     'DATABASE_URI' : 'postgresql://postgres:12230@localhost:5432/project-2.4'
 # }
-# config = {
-#     'DATABASE_URI' : 'postgresql://postgres:642251s@localhost:5432/artestify'
-# }
 config = {
-    'DATABASE_URI' : 'postgresql://postgres:artestify@localhost:5432/artestify'
+    'DATABASE_URI' : 'postgresql://postgres:642251s@localhost:5432/artestify'
 }
+# config = {
+#     'DATABASE_URI' : 'postgresql://postgres:artestify@localhost:5432/artestify'
+# }
 # config = {
 #     'DATABASE_URI' : 'postgresql://postgres:Pieter1998@localhost:5432/project-2.4'
 # }
