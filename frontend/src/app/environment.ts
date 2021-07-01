2212222632ccf27074a9363d375d31e22e722354
Douwe Pieter Reitsma <d.p.reitsma@gmail.com>
2021-07-01 14:20:45 +0200

    https

--- frontend/src/app/environment.ts
+++ frontend/src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 
-    apiUrl: 'http://127.0.0.1:8000',
+    apiUrl: 'https://127.0.0.1:8000',
        // <-- HERE PUT OTHER CONFIG THAT CAN CHANGE FROM ENVIROMENT
   };
