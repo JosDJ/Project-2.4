9d7d0c5338345fea95bc91c8876f983de9402e31
lordhessel2 <h.van.der.heide@st.hanze.nl>
2021-07-01 14:29:28 +0200

    zoeken beetje verbeterd

--- frontend/src/app/mainscreen/zoeken/zoeken.component.ts
+++ frontend/src/app/mainscreen/zoeken/zoeken.component.ts
@@ -11,6 +11,8 @@ export class ZoekenComponent implements OnInit {
   zoekterm:string= '';
   gevondenSongs:string[] = [];
   gevondenAlbums:string[] = [];
+  error!: string;
+  errorAlbums!: string;
 
   constructor(private dataParser: ApiService) { }
 
@@ -18,18 +20,30 @@ export class ZoekenComponent implements OnInit {
   }
 
   search(): void {
+    this.error = '';
+    this.errorAlbums = '';
     this.gevondenSongs = [];
     this.gevondenAlbums = [];
-    this.dataParser.getallSongsByTitle(this.zoekterm).subscribe(songs => {
-      for(let i=0; i<songs.length; i++){
-        this.gevondenSongs.push(songs[i].title);
-      }
-    })
-    this.dataParser.getallAlbumsByTitle(this.zoekterm).subscribe(albums => {
-      for(let i=0; i<albums.length; i++){
-        this.gevondenAlbums.push(albums[i].title);
-      }
-    })
+    if (this.zoekterm == ''){
+
+    }else{
+      this.dataParser.getallSongsByTitle(this.zoekterm).subscribe(songs => {
+        for(let i=0; i<songs.length; i++){
+          this.gevondenSongs.push(songs[i].title);
+        }
+      },
+      (error: string) => {
+        this.error = 'no songs found';
+      })
+      this.dataParser.getallAlbumsByTitle(this.zoekterm).subscribe(albums => {
+        for(let i=0; i<albums.length; i++){
+          this.gevondenAlbums.push(albums[i].title);
+        }
+      },
+      (errorAlbums: string) => {
+        this.errorAlbums = 'no albums found';
+      })
+    }  
   }
 
 }
